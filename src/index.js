d92feb72781674acd779829be386f3280acb7ddf
Sergey Dvaradkin <dvoryadkin1989@gmail.com>
2020-02-17 09:43:46 +0300

    feat: add status endpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get("/api/v0.1/status", (req, res) => {
+    res.send({
+        status: "ok"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`app is ready on port ${port}`);
+});
